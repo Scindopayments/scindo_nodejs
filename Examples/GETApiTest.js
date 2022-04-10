b86b92e6f898247d1d95126ba71d300cecdca88d
scindopayments <scindopayments@gmail.com>
2022-04-10 17:04:29 +0200

    Error catching

--- Examples/GETApiTest.js
+++ Examples/GETApiTest.js
@@ -5,19 +5,34 @@ let usernameBasic = username;
 let passwordBasic = password;
 
 function getUsers() {
-    try {
         return axios.get('https://api.yapily.com/users', {
             auth: {
                 username : usernameBasic,
                 password : passwordBasic
             }
         })
-    }
-    catch (err) {
-        console.log(err)
-    }
+            .then(rs => {
+                const data = rs.data;
+                data.retcode = rs.status;
+                return  data;
+            })
+            .catch(err => err?.response?.data?.error || err)
+
 }
-const data = (await getUsers()).data;
-console.log(data);
-console.log(data[0]);
-console.log(data[0].uuid);
+
+const data = await getUsers();
+
+//OK CALL
+if(data.retcode) {
+    console.log(data); //GET the entire response
+    console.log(data[0]); //GET the first object
+    console.log(data[0].uuid); //GET the uuid of the first object
+}
+
+//ERROR CALL
+if(data.code){
+    console.log(data);
+    console.log(data.code);
+    console.log(data.message);
+}
+
--- Examples/POSTApiTest.js
+++ Examples/POSTApiTest.js
@@ -4,19 +4,34 @@ import axios from 'axios';
 let usernameBasic = username;
 let passwordBasic = password;
 
+let userID = 'APPLICATION_USER_ID17'
+
 function createUser() {
-    try {
-        return axios.post('https://api.yapily.com/users', {applicationUserId: 'APPLICATION_USER_ID9'}, {
-            auth: {
-                username : usernameBasic,
-                password : passwordBasic
-            }
+    return axios.post('https://api.yapily.com/users', {applicationUserId: userID}, {
+        auth: {
+            username : usernameBasic,
+            password : passwordBasic
+        }
+    })
+        .then(rs => {
+            const data = rs.data;
+            data.retcode = rs.status;
+            return  data;
         })
-    }
-    catch (err) {
-
-    }
+        .catch(err => err?.response?.data?.error || err)
 }
-const data = (await createUser()).data;
+const data = await createUser();
 
-console.log(data.error);
+//OK CALL
+if(data.retcode) {
+    console.log(data);
+    console.log(data.retcode);
+}
+
+//ERROR CALL
+if(data.code){
+    console.log(data);
+    console.log(data.code);
+    console.log(data.message);
+}
+

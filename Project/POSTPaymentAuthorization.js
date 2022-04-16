230b0f4bcb8d53dd6fa2a3c89db62192cbca5705
scindopayments <scindopayments@gmail.com>
2022-04-16 03:34:01 +0200

    payment-auth-requests

--- Project/POSTPaymentAuthorization.js
+++ Project/POSTPaymentAuthorization.js
@@ -5,63 +5,67 @@ let usernameBasic = username;
 let passwordBasic = password;
 
 
-const applicationUserId = 'single-payment-tutorial';
-const callback = 'https://scindopayments/callback1'
 
+//Body creator in Json
+function jsonBodyParser() {
 
+    const applicationUserId = 'single-payment-tutorial';
+    const callback = 'https://scindopayments/callback1'
+    let institutionId = 'bpm-sandbox';
 
-let institutionId = 'bpm-sandbox';
+    //paymentRequest
+    let paymentRequest_type = 'DOMESTIC_PAYMENT';
+    let paymentRequest_reference = 'Bills Coffee Shop';
+    let paymentRequest_paymentIdempotencyId = '1' //ID;
+    //Amount
+    let amount_amount = 9.00;
+    let amount_currency = "EUR";
+    //paymentRequestPayee
+    let payee_name = 'BILLS COFFEE LTD';
+    //address
+    let country = 'IT';
 
-//paymentRequest
-let paymentRequest_type = 'DOMESTIC_PAYMENT';
-let paymentRequest_reference = 'Bills Coffee Shop';
-let paymentRequest_paymentIdempotencyId = '1' //ID;
-//Amount
-let amount_amount = 9.00;
-let amount_currency = "EUR";
-//paymentRequestPayee
-let payee_name = 'BILLS COFFEE LTD';
-//address
-let country = 'IT';
-
-let body = JSON.stringify({
-    applicationUserId: applicationUserId,
-    institutionId: institutionId,
-    callback : callback,
-    paymentRequest : {
-        type : paymentRequest_type,
-        reference : paymentRequest_reference,
-        paymentIdempotencyId : paymentRequest_paymentIdempotencyId,
-        amount : {
-            amount : amount_amount,
-            currency :amount_currency
-        },
-        payer : {
-            name : 'gigi',
-            accountIdentifications :  [
-                {
-                    type: "IBAN",
-                    identification: "DE12345678901234567890"
+    let body = JSON.stringify({
+        applicationUserId: applicationUserId,
+        institutionId: institutionId,
+        callback: callback,
+        paymentRequest: {
+            type: paymentRequest_type,
+            reference: paymentRequest_reference,
+            paymentIdempotencyId: paymentRequest_paymentIdempotencyId,
+            amount: {
+                amount: amount_amount,
+                currency: amount_currency
+            },
+            payer: {
+                name: 'gigi',
+                accountIdentifications: [
+                    {
+                        type: "IBAN",
+                        identification: "DE12345678901234567890"
+                    }
+                ]
+            },
+            payee: {
+                name: payee_name,
+                accountIdentifications: [
+                    {
+                        type: "IBAN",
+                        identification: "BE12345678901234"
+                    }
+                ],
+                address: {
+                    country: country
                 }
-            ]
-        },
-        payee : {
-            name : payee_name,
-            accountIdentifications : [
-                {
-                    type: "IBAN",
-                    identification: "BE12345678901234"
-                }
-            ],
-            address : {
-                country : country
             }
         }
-    }
-});
+    });
 
-function postPaymentAuthorization() {
-    return axios.post('https://api.yapily.com/payment-auth-requests', body, {
+    return body;
+}
+
+function postPaymentAuthorization(jsonBody) {
+    return axios.post('https://api.yapily.com/payment-auth-requests', jsonBody, {
         auth: {
             username : usernameBasic,
             password : passwordBasic,
@@ -78,7 +82,10 @@ function postPaymentAuthorization() {
         })
         .catch(err => err?.response?.data?.error || err)
 }
-const data = await postPaymentAuthorization();
+
+
+let jsonBody = jsonBodyParser();
+let data = await postPaymentAuthorization(jsonBody);
 
 //OK CALL
 if(data.retcode) {
